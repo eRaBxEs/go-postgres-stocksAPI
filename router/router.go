872084604064d20af5128af3d4e7f34af3beca29
Ehioje Henry Erabor <ehiojee@yahoo.com>
2023-01-09 13:16:42 +0100

    adjust the routes

--- router/router.go
+++ router/router.go
@@ -9,11 +9,11 @@ import (
 func Router() *mux.Router {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/stock/{:id}", middleware.GetStock).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/stock/{id}", middleware.GetStock).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/stock", middleware.GetAllStocks).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/newstock", middleware.CreateStock).Methods("POST", "OPTIONS")
-	router.HandleFunc("/api/stock/{:id}", middleware.UpdateStock).Methods("PUT", "OPTIONS")
-	router.HandleFunc("/api/deletestock/{:id}", middleware.DeleteStock).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/api/stock/{id}", middleware.UpdateStock).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/deletestock/{id}", middleware.DeleteStock).Methods("DELETE", "OPTIONS")
 
 	return router
 }
